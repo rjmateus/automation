e4b8116aaf90070ef2c7d55eee1e2525df7e6a26
Ricardo Mateus <rmateus@suse.com>
2020-03-25 08:49:40 +0000

    my first commit
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>

---
+++ backups/backup_suse_notes.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# backup of suse notes project
+
+
+function backup_folder() {
+
+  echo "processing folder '$1'"
+  cd $1
+  git diff --exit-code
+  if [ $? -ne 0 ]; then
+    echo "changes to commit"
+    git add -A
+    TODAY=`date +"%m-%d-%y-%T"`
+    git commit -s -S -m "$TODAY"
+  fi
+
+  git push
+
+}
+
+declare -a project_folders=('/home/rmateus/projects/SUSE_notes')
+#'/home/rmateus/projects/scripts/ric_automation')
+
+for i in "${project_folders[@]}"
+do
+  backup_folder $i
+done
+#cd '/home/rmateus/projects/SUSE_notes'
+#git diff --exit-code
+#if [ $? -ne 0 ]; then
+#  echo "changes to commit"
+#  git add -A
+#  TODAY=`date +"%m-%d-%y-%T"`
+#  git commit -s -S -m "$TODAY"
+#fi
+
+#git push
